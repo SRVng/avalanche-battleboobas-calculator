9a15c2479f8ac078d5f6322d8b6b92adb3877892
SRVng <snakglom@gmail.com>
2022-01-30 18:42:23 +0700

    init

--- config/index.tsx
+++ config/index.tsx
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://avalanche-battleboobas-calculator-n2hllq13b-srvng.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://avalanche-battleboobas-calculator-srvng.vercel.app';
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ["localhost","avalanche-battleboobas-calculator-srvng.vercel.app"]
+  },
+  experimental: {
+    urlImports: ["http://localhost:3000","https://avalanche-battleboobas-calculator-srvng.vercel.app"]
+  }
+  
 }
 
 module.exports = nextConfig
