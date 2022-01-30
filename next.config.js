44ca05c8cada32eec913072628fe84da080c7a04
SRVng <snakglom@gmail.com>
2022-01-30 19:28:07 +0700

    add domains

--- next.config.js
+++ next.config.js
@@ -5,12 +5,14 @@ const nextConfig = {
     domains: [
       "localhost",
       "avalanche-battleboobas-calculator-srvng.vercel.app",
+      "avalanche-battleboobas-calculator.vercel.app"
     ]
   },
   experimental: {
     urlImports: [
       "http://localhost:3000",
       "https://avalanche-battleboobas-calculator-srvng.vercel.app",
+      "https://avalanche-battleboobas-calculator.vercel.app"
     ]
   }
   
