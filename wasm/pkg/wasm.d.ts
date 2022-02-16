8640cf85c10f9f09b3b2cd6098fafd3d4054eea4
SRVng <snakglom@gmail.com>
2022-02-17 03:09:13 +0700

    pkg

---
+++ wasm/pkg/wasm.d.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} milk
+* @param {number} size
+* @param {number} prestige
+* @returns {number}
+*/
+export function calculate_reward(milk: number, size: number, prestige: number): number;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly calculate_reward: (a: number, b: number, c: number) => number;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
